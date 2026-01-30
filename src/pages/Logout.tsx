1fbf3981cbd9689bd054817a0ef39a376fac44a1
ThisIsJanAdrian <thisisjanadrian@gmail.com>
2026-01-30 14:22:34 +0800

    Added authentication functions with Supabase

--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -1,3 +1,27 @@
+import { use, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { supabase } from '../services/supabaseClient';
+import { authSlice } from '../app/authSlice';
+
 export default function Logout() {
-    return <h1>Logout Page</h1>
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const logoutUser = async () => {
+            await supabase.auth.signOut();
+            dispatch(authSlice.actions.logout());
+            console.log('User logged out successfully');
+            navigate('/login');
+        };
+
+        logoutUser();
+    }, []);
+
+    return (
+        <div>
+            <h1>Logging out...</h1>
+        </div>
+    );
 }
