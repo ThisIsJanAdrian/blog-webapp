1f12d08684f14b786c4cd640f544be3c4b0e908a
ThisIsJanAdrian <thisisjanadrian@gmail.com>
2026-01-30 15:10:47 +0800

    Added persistent login

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux';
+import { supabase } from './services/supabaseClient';
+import { authSlice } from './app/authSlice';
 import AppRoutes from './AppRoutes';
 
 export default function App() {
+    const dispatch = useDispatch();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const checkUser = async () => {
+            const { data: { user } } = await supabase.auth.getUser();
+            if (user) {
+                dispatch(authSlice.actions.login({ email: user.email! }));
+            }
+            else {
+                dispatch(authSlice.actions.logout());
+            }
+            setLoading(false);
+        };
+
+        checkUser();
+    }, []);
+
+    if (loading) {
+        return <p>Loading...</p>;
+    }
     return <AppRoutes />;
 }
